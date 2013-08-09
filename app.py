1c0693ee65b0ac67c80aed5c7d93cf8c4e79ee57
shaunokeefe <shaun.okeefe.0@gmail.com>
2013-08-09 08:27:20 +0000

    display clustered tweets on map

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask import render_template
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
         return 'Hello World!'
 
-@app.route('/heatmap/')
-def heat_map(name=None):
-    return render_template('heatmap.html', name=name)
+@app.route('/map/')
+def map(name=None):
+
+    db_name = 'tweet_db'
+    collection_name = 'user_collection'
+    db_client = MongoClient()
+    db = db_client[db_name]
+    collection = db[collection_name]
+
+    users = collection.find()
+    return render_template('map.html', users=users)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
